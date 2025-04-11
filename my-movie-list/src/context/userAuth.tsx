858a995589999c802ebd76f430fd5c9ba56d7fb5
ericlong-info <eric.long@infosys.com>
2025-04-11 09:11:13 -0400

    login functionality and logout

---
+++ my-movie-list/src/context/userAuth.tsx
@@ -0,0 +1,95 @@
+import React, { createContext, useEffect, useState } from "react";
+import { User } from "../models/users";
+import { useNavigate } from "react-router-dom";
+import { loginAPI, registerAPI } from '../services/authService';
+import axios from "axios";
+import { toast } from "react-toastify";
+
+type UserContextType = {
+    user: User | null;
+    token: string | null;
+    registerUser: (email: string, username: string, password: string) => void;
+    loginUser: (username: string, password: string) => void;
+    logout: () => void;
+    isLoggedIn: () => boolean;
+};
+
+type Props = { children: React.ReactNode };
+
+const UserContext = createContext<UserContextType>({} as UserContextType);
+
+export const UserProvider = ({ children } : Props) => {
+
+    const navigate = useNavigate();
+    const [token, setToken] = useState<string | null>(null);
+    const [user, setUser] = useState<User | null>(null);
+    const [isReady, setIsReady] = useState(false);
+
+    useEffect(() => {
+        const user = localStorage.getItem("user");
+        const token = localStorage.getItem("token");
+        if(user && token) {
+            setUser(JSON.parse(user));
+            setToken(token);
+            axios.defaults.headers.common["Authorization"] = "Bearer " + token;
+        }
+        setIsReady(true);
+    }, []);
+
+    const registerUser = async (email: string, username: string, password: string) => {
+        await registerAPI(email, username, password).then((res) => {
+            if(res) {
+                localStorage.setItem("token", res?.data.token);
+                const userObj = {
+                    username: res?.data.username,
+                    email: res?.data.email,
+              };
+              localStorage.setItem("user", JSON.stringify(userObj));
+              setToken(res?.data.token);
+              setUser(userObj!);
+              toast.success("Registration Successful!");
+              navigate("/");
+            }
+        }).catch((e) => toast.warning("Server error occured"));
+    };
+
+    const loginUser = async (username: string, password: string) => {
+        await loginAPI(username, password).then((res) => {
+            if(res) {
+                localStorage.setItem("token", res?.data.token);
+                const userObj = {
+                    username: res?.data.username,
+                    email: res?.data.email,
+              };
+              localStorage.setItem("user", JSON.stringify(userObj));
+              setToken(res?.data.token);
+              console.log(token);
+              setUser(userObj!);
+              toast.success("Login Successful!");
+              navigate("/");
+            }
+        }).catch((e) => toast.warning("Server error occured"));
+    };
+
+    const isLoggedIn = () => {
+        return !user;
+    };
+
+    const logout = () => {
+        localStorage.removeItem("token");
+        localStorage.removeItem("user");
+        setUser(null);
+        setToken("");
+        navigate("/");
+    }
+
+    return (
+        <UserContext.Provider value={{loginUser,registerUser,isLoggedIn,logout}}
+        >
+            {isReady ? children : null}
+        </UserContext.Provider>
+    )
+};
+
+export const useAuth = () => React.useContext(UserContext);
+
