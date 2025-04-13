7b427afa6bb4cced8c8f5ebd826e243559fd44eb
Steven <stevenmdmcleod@gmail.com>
2025-04-12 20:51:25 -0400

    added back watchlists route

--- my-movie-list/src/App.tsx
+++ my-movie-list/src/App.tsx
@@ -58,6 +58,7 @@ function App() {
             <Route path="/login" element={<Login />} />
             <Route path="/registration" element={<Registration />} />
             <Route path="/friends" element={<Friends />} />
+            <Route path="/watchlists" element={<Watchlists />} />
             <Route path="/watchlist/:listId" element={<Watchlists />} />
             <Route element={<ProtectedRoute />}>
               <Route path="/profile" element={<Profile />} />
